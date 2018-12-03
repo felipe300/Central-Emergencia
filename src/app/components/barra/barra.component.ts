fd695f3d78f348c2462ed46e8ecae567640af5c5
tenosb <tenobot@gmail.com>
2018-12-03 02:38:04 -0300

    update style

--- src/app/components/barra/barra.component.ts
+++ src/app/components/barra/barra.component.ts
@@ -17,12 +17,12 @@ export class BarraComponent {
   public barChartLegend:boolean = true;
  
   public barChartData:any[] = [
-    {data: [37, 44, 42, 45, 18, 6, 8, 11, 9, 17, 16, 37], label: '10-05-09 QUEMA DE MATORRALES'},
-    {data: [11, 17, 10, 20, 15, 28, 13, 27, 29, 19, 16, 21], label: '10-06 ACCIDENTE DE TRANSITO'},
+    {data: [37, 44, 42, 45, 18, 6, 8, 11, 9, 17, 16, 37], label: '10-2 QUEMA DE MATORRALES'},
+    {data: [11, 17, 10, 20, 15, 28, 13, 27, 29, 19, 16, 21], label: '10-4-1 ACCIDENTE DE TRANSITO'},
     {data: [37, 44, 42, 20, 15, 13, 11, 36, 39, 38, 42, 40], label: '10-09-05 REBROTE DE INCENDIO'},
     {data: [10, 10, 8, 5, 8, 6, 11, 7, 9, 9, 6, 7], label: '10-10 INCENDIO DECLARADO'},
-    {data: [28, 28, 20, 19, 36, 47, 40, 54, 54, 26, 27, 29], label: '10-10-01 INCENDIO DECLARADO SECTOR RURAL'},
-    {data: [15, 19, 18, 18, 16, 15, 14, 18, 17, 19, 15, 17], label: '10-10-02 INCENDIO DECLARADO DE MATERIAS PELIGROSAS'}
+    {data: [28, 28, 20, 19, 36, 47, 40, 54, 54, 26, 27, 29], label: '10-0-1 INFLAMACIÓN DE CHIMENEA'},
+    {data: [15, 19, 18, 18, 16, 15, 14, 18, 17, 19, 15, 17], label: '10-10-02 INCENDIO DECLARADO CON MATERIALES PELIGROSOS'}
   ];
  
   // events
